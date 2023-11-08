ac457bef719889a8df3d7aecbc4b36683b44abd6
ms666 <1550324080@qq.com>
2023-11-08 21:29:43 +0800

    实现用户管理、角色管理

---
+++ grace-console-ui/src/store/modules/form.js
@@ -0,0 +1,38 @@
+const state= {
+
+    //表单（是否启用）开关，默认是true打开
+    curSwitchStatus:true
+
+}
+
+const mutations = {
+
+    SET_UPLOAD_AVATAR(state,avatarPath){
+        state.uploadAvatar=avatarPath
+    }
+    ,
+    SET_CURSWITCHSTATUS(state,curSwitchStatus){
+        state.curSwitchStatus=curSwitchStatus
+    }
+
+
+}
+
+const actions={
+
+    setUploadAvatar(context,avatarPath){
+        context.commit('SET_UPLOAD_AVATAR',avatarPath)
+    }
+    ,
+    setCurSwitchStatus(context,curSwitchStatus){
+        context.commit('SET_CURSWITCHSTATUS',curSwitchStatus)
+    }
+
+}
+
+
+export default {
+    state,
+    mutations,
+    actions
+}
